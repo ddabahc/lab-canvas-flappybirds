5d0e66581936f1e3046d950d664c70fd98807739
Daniel <DDABAHC89@GMAIL.COM>
2021-10-30 11:47:32 -0500

    add functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 window.onload = function() {
+
+  //llamar mis clases
+  const bg = new Background(canvas.width,canvas.height)
+  const flappy = new Flappy(20,40,30,30)
+
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
+    requestId = requestAnimationFrame(update)
+  }
 
+  function gameOver(){}
+
+  function update(){
+    frames ++;
+    // limpiar el canvas
+    ctx.clearRect (0,0, canvas.width,canvas.height)
+    bg.draw()
+    flappy.draw()
+  
+    if (requestId){
+      requestId = requestAnimationFrame(update)
+      
+    }
   }
 
 };
